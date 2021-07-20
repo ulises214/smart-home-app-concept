ea2102efed5302a8d0e6526eee8b3288f121aaf9
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-07-20 11:37:21 -0500

    Document colors

--- lib/view/constants/colors.dart
+++ lib/view/constants/colors.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 const _black = Color.fromRGBO(20, 20, 26, 1);
 const _white = Color.fromRGBO(247, 246, 248, 1);
 
+/// Define all color that will used in the app
 abstract class WalleColors {
+  /// The theme to style the app in dark mode
   static final darkTheme = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: _black,
     tabBarTheme: ThemeData.dark().tabBarTheme.copyWith(
@@ -11,6 +13,8 @@ abstract class WalleColors {
           unselectedLabelColor: _white.withAlpha((255 * 0.4).toInt()),
         ),
   );
+
+  /// The theme to style the app in light mode
   static final lightTheme = ThemeData.light().copyWith(
     scaffoldBackgroundColor: _white,
     tabBarTheme: ThemeData.light().tabBarTheme.copyWith(
