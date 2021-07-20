884605b91916aaced2c9162580c2398450e610ce
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-07-20 09:21:16 -0500

    Basic app created

---
+++ android/app/src/main/kotlin/com/example/walles_smart_home/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.walles_smart_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Cupertino App',
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Cupertino App Bar'),
+        ),
+        child: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
